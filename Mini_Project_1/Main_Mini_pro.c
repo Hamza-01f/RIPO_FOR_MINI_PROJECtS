0a344b21b3fb8d96f19e5ae78f0f67d9ddf54538
Hamza <hamzaprograme@gmail.com>
2024-09-09 17:16:16 +0100

    faires quelques echange sur le mini-project

--- Mini_Project_1/Main_Mini_pro.c
+++ Mini_Project_1/Main_Mini_pro.c
@@ -7,18 +7,19 @@ int Nbr_De_Liv = 0;
 //-------------------------------------- function d'ajouter -----------------------------
 void Ajoute_Une_livre( char title[][20] ,
     char auteur[][20] ,
-    float price[] , int quantity[])
+    int price[] , int quantity[])
     {
      if(Nbr_De_Liv <= MAX){
 
+
       printf("Entrez le titre de livre : ");
       scanf(" %[^\n]",title[Nbr_De_Liv]);
-
+    
       printf("Entrez l'auteur de livre : ");
       scanf(" %[^\n]",auteur[Nbr_De_Liv]);
 
       printf("Entrez le prix de livre : ");
-      scanf("%f",&price[Nbr_De_Liv]);
+      scanf("%d",&price[Nbr_De_Liv]);
 
       printf("Entrez la quantité de livre : ");
       scanf("%d",&quantity[Nbr_De_Liv]);
@@ -34,7 +35,7 @@ void Ajoute_Une_livre( char title[][20] ,
 
 //-------------------------------------function d'afficher-------------------------------
 void Affiche_livres_dispo(char title[][20], 
-        char auteur[][20], float price[],
+        char auteur[][20], int price[],
         int quantity[])
         {
          if(Nbr_De_Liv == 0){
@@ -48,7 +49,7 @@ void Affiche_livres_dispo(char title[][20],
             printf("Livre %d :\n",i+1);
             printf("Titre : %s\n",title[i]);
             printf("Auteur : %s\n",auteur[i]);
-            printf("Prix : %.2f DH\n",price[i]);
+            printf("Prix : %d DH\n",price[i]);
             printf("Quantité  : %d\n",quantity[i]);
             printf("-------------------\n");
       }
@@ -56,43 +57,36 @@ void Affiche_livres_dispo(char title[][20],
 }
 
 //-----------------------------------function d mettre ajour le stock -------------------
-void Update_stock(char titre[][20] , int Quant[])
+void Update_stock(char titre[][20],int Quant[])
 {
    if(Nbr_De_Liv > 0){
-        int check = 0;
+        
+        int exact_indix;
+        int call ;
 
        do{
            char Tit_Ajour[20];
            int val_Ajour;
-           int count = 0;
 
            printf("Entrez Le Titre De Livre Pour Mettre a Jour sa Quantité : ");
            scanf(" %[^\n]",Tit_Ajour);
 
-           printf("Entrez La Nouvelle Valeur De Sa Quantité : ");
-           scanf("%d",&val_Ajour);
+           call = Verify_existance(titre,Tit_Ajour,&exact_indix);
 
-           int leng = strlen(Tit_Ajour);
+           if(call == 1){
+               printf("Entrez La Nouvelle Valeur De Sa Quantité : ");
+               scanf("%d",&val_Ajour);
 
-           for(int i = 0 ; i < Nbr_De_Liv ; i++){//pass sur les titres
-                for(int j = 0 ; titre[i][j] != '\0' ; j++){//pass sur chaque titre
-                    if(titre[i][j] == Tit_Ajour[j])//comparison entre les deux titre
-                        count++;
-                    else{
-                        check++;//si le titre entreé par l'étulisateur et le titre stocké dans le tableau pas le mémé
-                        break;
-                   }
-                   if( count == leng){
-                        Quant[i] = val_Ajour;//changer le precedent quantité par nouvelle valeur
-                        printf("La Quantité de Livre donné Mettre a jour avec succe:\n");
-                   }
-                 }
-              }
-             if(check == Nbr_De_Liv){
+               Quant[exact_indix] = val_Ajour;//changer le precedent quantité par nouvelle valeur
+
+              printf("La Quantité de Livre donné Mettre a jour avec succe:\n");
+
+              return;
+           }else{
                         printf("\nLe Titre Donné est pas existe dans le stock : \n");
                         printf("\nEssayez encore Par Une Correct Titre : \n");
              }
-        }while(check == Nbr_De_Liv);
+        }while( call != 1);
     }else{
         printf("\nLe Stock est Vide Rien de Mettre a Jour :\n");
     }
@@ -100,12 +94,14 @@ void Update_stock(char titre[][20] , int Quant[])
 
 void Suppri_livre();
 void Display_nmr_total(); 
+void Recherche_Livre();
+int  Verify_existance();
 
 // la function int main commance sa execution
 int main(){                       //_______
     char Tit_liv[MAX][20];        //       | 
     char Auteu_liv[MAX][20];      //       |------>declarer les tableau principal
-    float Prix[MAX];              //       |
+    int Prix[MAX];              //       |
     int Quant[MAX];               //_______| 
                                    
 
@@ -120,10 +116,11 @@ int main(){                       //_______
    printf("| |--------------[Voici le menu principal]-----------------|      |\n");
    printf("|_1). Ajouter un livre au stock.:                                 |\n");
    printf("|_2). Afficher tous les livres disponibles. :                     |\n");
-   printf("|_3). Mettre à jour la quantité d'un livre. :                    |\n");
+   printf("|_3). Mettre à jour la quantité d'un livre. :                     |\n");
    printf("|_4). Supprimer un livre du stock. :                              |\n");
    printf("|_5). Afficher le nombre total de livres en stock.:               |\n");
-   printf("|_6). Quitter :                                                   |\n");
+   printf("|_6). Rechercher un livre . :                                     |\n");
+   printf("|_7). Quitter :                                                   |\n");
    printf("|_________________________________________________________________|\n");
    printf("\n");
    printf("Utilisez les nombres pour choisir votre choix :\n");
@@ -149,12 +146,15 @@ int main(){                       //_______
         Display_nmr_total(Quant);//la function pour afficher le nombre total de tous les livres
         break;
     case 6:
-         printf("Passe une bonne journée!:\n");
+        Recherche_Livre(Tit_liv,Auteu_liv,Prix,Quant);//la function pour rechercher
+        break;
+    case 7:
+         printf("Passe une bonne journée!:\n");//pour quitter
          break;
     default:
         printf("Votre choix est invalide. Essayer encore :\n");
     }
-    }while (choix != 6);
+    }while (choix != 7);//stop le menu
  
 
  return 0;
@@ -162,30 +162,23 @@ int main(){                       //_______
 
 //---------------------------------function de supprimer ---------------------------------
 void Suppri_livre(char titre[][20],
-  char auteur[][20],float price[],int quantity[]){
+  char auteur[][20],int price[],int quantity[]){
 
    if(Nbr_De_Liv > 0){
-        int check = 0;
+        int call;
+        int exact_indix = 0;
 
        do{
            char Tit_suppr[20];
-           int count = 0;
 
            printf("Entrez Le Titre De Livre Pour La Suppreme : ");
-           scanf(" %[^\n]",Tit_suppr);
+           scanf(" %[^\n]",Tit_suppr);//l'entrée de le livre 
+        
+           call = Verify_existance(titre,Tit_suppr,&exact_indix);
 
-           int leng = strlen(Tit_suppr);
-
-           for(int i = 0 ; i < Nbr_De_Liv ; i++){//pass sur les titres
-                for(int j = 0 ; titre[i][j] != '\0' ; j++){//pass sur chaque titre
-                    if(titre[i][j] == Tit_suppr[j])//comparison entre les deux titres
-                        count++;
-                    else{
-                        check++;//si le titre entreé par l'étulisateur et le titre stocké dans le tableau pas le mémé
-                        break;
-                    }if( count == leng){
+                      if( call == 1){
                         Nbr_De_Liv--;
-                         for(int i = check ; i <= Nbr_De_Liv ; i++){
+                         for(int i = exact_indix ; i <= Nbr_De_Liv ; i++){
                              for(int j = 0 ; titre[i][j] != '\0' ; j++){
                                     titre[i][j] = titre[i + 1][j];
                                 }
@@ -193,24 +186,19 @@ void Suppri_livre(char titre[][20],
                                     auteur[i][j] = auteur[i + 1][j];
                                 }
                             }
-                            for(int j = check ; j <= Nbr_De_Liv ; j++){
+                            for(int j = exact_indix ; j <= Nbr_De_Liv ; j++){
                                     price[j] = price[j+1];
                                 }
-                            for(int j = check ; j <= Nbr_De_Liv ; j++){
+                            for(int j = exact_indix ; j <= Nbr_De_Liv ; j++){
                                     quantity[j] = quantity[j+1];
                                 }
                          printf("\nLe Livre Supprime Avec succe : \n");
                          return;
-                     }
-
-                  }
-               }
-
-             if(check == Nbr_De_Liv){
-                        printf("\nLe Titre Donné est pas existe Dans le Stock : \n");
-                        printf("\nEssayez encore Par Une Correct Titre: \n");
-             }
-        }while(check == Nbr_De_Liv);
+                       }else{
+                         printf("\nLe Titre Donné est pas existe Dans le Stock : \n");
+                         printf("\nEssayez encore Par Une Correct Titre: \n");
+                       }
+        }while(call != 1);
     }else{
         printf("\nLe Stock est Vide Rien De Supprimer :\n");
     }
@@ -225,3 +213,60 @@ void Display_nmr_total(int quantity[]){
 
          printf("\nLe Nombre Total De Livres En Stock est : %d \n",somme);
 } 
+
+//-----------------------------------function de rechercher -------------------------------------------------------------
+void Recherche_Livre(char Titre[][20],char auteur[][20],int price[],int quantity[]){
+        if(Nbr_De_Liv > 0){
+         
+         int call ;
+         int exact_indix = 0 ;
+        do{
+           char Tit_rechercher[20];
+
+           printf("Entrez Le Titre De Livre Que Vous Voulez Cherchez : ");
+           scanf(" %[^\n]",Tit_rechercher);
+
+              call = Verify_existance(Titre,Tit_rechercher,&exact_indix);
+                      if(call == 1){
+                         printf("\nLe Livre Trouver Avec Succe : \n");
+                         printf("\nVoici les information de ce livre : \n");
+                         printf("Titre : %s\n",Titre[exact_indix]);
+                         printf("Auteur : %s\n",auteur[exact_indix]);
+                         printf("Prix : %d DH\n",price[exact_indix]);
+                         printf("Quantité  : %d\n",quantity[exact_indix]);
+                         printf("-------------------\n");
+                         return;
+                        }else{
+                        printf("\nLe Titre Donné est pas existe Dans le Stock : \n");
+                        printf("\nEssayez encore Par Une Correct Titre: \n");
+                        }
+         }while(call != 1);
+     }else
+          printf("\nLe Stock est Vide Rien De Rechercher :\n");
+
+  }
+
+//------------------------------ verifier si le titre exist ou non -------------------------------------
+int Verify_existance(char Titre[][20] ,char extra_table[],int *exact_ind){
+         int count = 0;
+         int check = 0;
+
+         int leng = strlen(extra_table);
+        for(int i = 0 ; i < Nbr_De_Liv ; i++){//pass sur les titres
+                for(int j = 0 ; Titre[i][j] != '\0' ; j++){//pass sur chaque titre
+                    if(Titre[i][j] == extra_table[j]){//comparison entre les deux titres
+                            count++;
+                      if( count == leng){
+                        *exact_ind = i;
+                         return 1;
+                        }
+                     
+                    }else{
+                        check++;//si le titre entreé par l'étulisateur et le titre stocké dans le tableau pas le méme
+                        break;
+                     }
+                  }
+               }
+        if(check != 0)
+        return 2;
+}
